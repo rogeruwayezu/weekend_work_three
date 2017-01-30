84f26a82079126e3042c1feb6f235a2c8707ff83
rogeruwayezu <rogeruwayezu@gmail.com>
2017-01-30 10:24:45 +0200

    answered the last question in oop

--- oop/tv_and_remote.rb
+++ oop/tv_and_remote.rb
@@ -11,35 +11,22 @@
 # decrement_volume (this will decrease the tv's volume by 1)
 # set_channel (this will change the tv's channel to whatever integer is passed to this method)
 
-class Tv
-  def initialize(tv_info)
-    @power = tv_info[:power]
-    @volume = tv_info[:volume]
-    @channel = tv_info[:channel]
-    @switch = [:active]
-  end
-
-end
-
-tv = Tv.new({power: 224, volume: 50, channel: 2})
-
-class Remote < Tv
+class Remote 
   def initialize(tv)
     @tv = tv
-    @switch = tv[:active]
   end
   def toggle_power
-    if tv.active == true
-      tv.active = false
-    elsif tv.active == false
-      tv.active = true
+    if @power == true
+       @power = false
+    elsif @power == false
+       @power = true
     end
   end
   def increment_volume
-    tv.volume += 1
+    volume += 1
   end
   def decrement_volume
-    tv.volume -= 1
+    volume -= 1
   end
   def set_channel(number)
     tv.channel = number
@@ -47,4 +34,30 @@ class Remote < Tv
 
 end
 
-remote = Remote.new(tv_1)
+class Tv < Remote
+  def initialize(tv_info)
+    @power = tv_info[:power]
+    @volume = tv_info[:volume]
+    @channel = tv_info[:channel]
+    
+  end
+
+end
+
+tv = Tv.new({power: true, volume: 50, channel: 2})
+
+remote = Remote.new("tv")
+
+result = tv.toggle_power
+puts "Add a fridge returned:"
+puts result
+puts
+
+if result == false
+  puts "PASS!"
+else
+  puts "F"
+end
+puts
+
+result = tv.
